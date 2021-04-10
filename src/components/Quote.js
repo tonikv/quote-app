51d365033c06da4d0f79356a884b5d0c6b13a03b
Toni Virtanen <toniKoodaa@gmail.com>
2021-04-10 10:43:26 +0300

    fix css

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Quote = ( {random, author, quote} ) => {
     return (
         <div className="centered" id="quote-box">
-            <h2 id="text">{quote}</h2>
+            <h3 id="text">{quote}</h3>
             <p id="author">{author}</p>
             <div className="below-parent">
                 <button id="new-quote" onClick={random}>New Quote</button>
